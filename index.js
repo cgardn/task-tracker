e5451a883abc164e7800bdbed88ebb59a1b4be37
Chris G <cgardn@users.noreply.github.com>
2024-08-06 11:36:56 -0400

    settings sidebar, color settings, more styling

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ Bugs:
 - bug: adding an item doesn't use the input, or clear it
 
 Todo Features:
+- move new list to ghost card w/ dotted border on left if no cards, as smaller skinny ghost card if some cards
+- move new item to top of list cards
+- fix edit mode for items, add some kind of edit for card titles, will need confirm/cancel buttons which can probably be re-used for delete confirmation
 - settings sidebar
   - themes, colors
-  - export/import to json
+    + top bar, background, cards, text
+    - include some palettes with the palette generator, and the ability to make a new theme/palette (ez js object)
+  - export/import to json, including settings
+  - add whole state to local storage
 - Drag and Drop
 
 Todo styling:
@@ -14,22 +20,7 @@ Todo styling:
 
 */
 
-(function() {
   /* app */
-  function new_list_input_handler(inputElement) {
-    let list = document.createElement('tracker-list');
-    list.title = inputElement.value;
-    inputElement.closest('.app').querySelector('.app-tasks').appendChild(list)
-    inputElement.value = '';
-  }
-  document.getElementById('new-list-input').addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-      new_list_input_handler(e.target);
-    }
-  });
-  document.getElementById('new-list-button').addEventListener('click', (e) => {
-    new_list_input_handler(e.target.previousElementSibling)
-  });
 
   // Drag and Drop
   /*
@@ -47,7 +38,6 @@ Todo styling:
    * - for both intra and inter list dragging, if the drop is invalid just cancel the move
    * 
    * For all drag operations, set original item/list to display: none to hide it, if cancelling then just set it back to display: flex
-   */
   document.addEventListener('dragover', (e) => {
     e.preventDefault();
     console.log('dragover', e);
@@ -56,7 +46,77 @@ Todo styling:
   document.getElementById('list-container').addEventListener('drop', (e) => {
     console.log('drop', e);
   });
-})()
+   */
+
+/* sidebar */
+document.querySelector('.sidebar .open-button').addEventListener('click', () => {
+  document.querySelector('.sidebar').classList.toggle('open');
+});
+
+document.querySelectorAll('.sidebar input[type="color"]').forEach(el => {
+  el.addEventListener('change', (e) => {
+    document.documentElement.style.setProperty(`--${e.target.name}`, e.target.value);
+  });
+});
+
+
+/* app */
+class TrackerApp extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.template = document.getElementById('app-template').content;
+    this.appendChild(this.template.cloneNode(true));
+    this.setupEvents();
+  }
+
+  new_list_input_handler(inputElement) {
+    let list = document.createElement('tracker-list');
+    list.title = inputElement.value;
+    inputElement.closest('.app').querySelector('.app-tasks').appendChild(list)
+    inputElement.value = '';
+  }
+
+  setupEvents() {
+    this.querySelector('[data-input]').addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        this.new_list_input_handler(e.target);
+      }
+    });
+    this.querySelector('[data-add]').addEventListener('click', (e) => {
+      this.new_list_input_handler(e.target.previousElementSibling)
+    });
+  }
+
+  export() {
+    let exportedData = {}
+    this.querySelectorAll('tracker-list').forEach(list => {
+      exportedData[list.title] = [];
+      list.querySelectorAll('tracker-item').forEach(item => {
+        exportedData[list.title].push({ text: item.querySelector('span').innerHTML });
+      });
+    })
+    return JSON.stringify(exportedData);
+  }
+
+  import(data) {
+    this.querySelector('task-container').innerHTML = '';
+    const importedData = JSON.parse(data);
+    for (let list in importedData) {
+      let newList = document.createElement('tracker-list');
+      newList.title = list;
+      importedData[list].forEach(item => {
+        let newItem = document.createElement('tracker-item');
+        newItem.text = item.text;
+        newList.appendChild(newItem);
+      });
+      this.querySelector('task-container').appendChild(newList);
+    }
+  }
+
+}
 
 class TrackerList extends HTMLElement {
   constructor() {
@@ -140,6 +200,7 @@ class TrackerItem extends HTMLElement {
   }
 }
 
+customElements.define('tracker-app', TrackerApp);
 customElements.define('tracker-list', TrackerList);
 customElements.define('tracker-item', TrackerItem);
 
